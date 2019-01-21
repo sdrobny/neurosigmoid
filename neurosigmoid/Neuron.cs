09b29d0cd0121f0a80b8db942b138b213a7977d1
Marek Bałek <nutis5555@gmail.com>
2019-01-21 20:13:48 +0100

    Pierwsze klasy

---
+++ neurosigmoid/Neuron.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neurosigmoid
+{
+    class Neuron
+    {
+        public double Delta { get; set; }
+        public double Value { get; set; }
+        public double Bias { get; set; }
+        public List<Dendrite> Dendrites { get; set; }
+
+        public int DendrideCount { get { return Dendrites.Count; } }
+
+        public Neuron()
+        {
+            Random r = new Random(Environment.TickCount);
+            this.Bias = r.NextDouble();
+            this.Dendrites = new List<Dendrite>();
+        }
+    }
+
+}
