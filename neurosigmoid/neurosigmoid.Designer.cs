776c7972ba44b56ebf683d83a28021eb706cd990
sdrobny <szymondrobny@gmail.com>
2019-01-23 00:42:48 +0100

    nie wiem co robie

--- neurosigmoid/neurosigmoid.Designer.cs
+++ neurosigmoid/neurosigmoid.Designer.cs
@@ -28,21 +28,220 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.networkGraph = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rmsChart = new System.Windows.Forms.PictureBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.trainButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.networkGraph)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rmsChart)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // networkGraph
+            // 
+            this.networkGraph.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.networkGraph.Location = new System.Drawing.Point(11, 25);
+            this.networkGraph.Name = "networkGraph";
+            this.networkGraph.Size = new System.Drawing.Size(380, 238);
+            this.networkGraph.TabIndex = 0;
+            this.networkGraph.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Struktura sieci:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 437);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Wykres RMS:";
+            // 
+            // rmsChart
+            // 
+            this.rmsChart.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rmsChart.Location = new System.Drawing.Point(11, 558);
+            this.rmsChart.Name = "rmsChart";
+            this.rmsChart.Size = new System.Drawing.Size(761, 48);
+            this.rmsChart.TabIndex = 3;
+            this.rmsChart.TabStop = false;
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(18, 324);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(352, 23);
+            this.runButton.TabIndex = 4;
+            this.runButton.Text = "Uruchom sieć";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // trainButton
+            // 
+            this.trainButton.Location = new System.Drawing.Point(18, 408);
+            this.trainButton.Name = "trainButton";
+            this.trainButton.Size = new System.Drawing.Size(355, 23);
+            this.trainButton.TabIndex = 5;
+            this.trainButton.Text = "Trenuj sieć";
+            this.trainButton.UseVisualStyleBackColor = true;
+            this.trainButton.Click += new System.EventHandler(this.trainButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 274);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "5";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(157, 298);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "5";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ilosc neuronow - 1 warstwa:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 298);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ilosc neuronow - 2 warstwa:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(160, 379);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(213, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "10000";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 382);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ilosc cykli:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 359);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Częstotliwość uczenia";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(160, 356);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(213, 20);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Text = "0,9";
+            // 
+            // treeView1
+            // 
+            this.treeView1.HideSelection = false;
+            this.treeView1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.treeView1.Location = new System.Drawing.Point(398, 25);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.ShowPlusMinus = false;
+            this.treeView1.Size = new System.Drawing.Size(374, 527);
+            this.treeView1.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 44);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Reset sieci";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // neurosigmoid
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 618);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trainButton);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.rmsChart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.networkGraph);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "neurosigmoid";
             this.Text = "neurosigmoid";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.networkGraph)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rmsChart)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox networkGraph;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox rmsChart;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Button trainButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- neurosigmoid/neurosigmoid.cs
+++ neurosigmoid/neurosigmoid.cs
@@ -12,12 +12,31 @@ namespace neurosigmoid
 {
     public partial class neurosigmoid : Form
     {
+        private List<double> input;
+        private List<double> output;
+        private NeuralNetwork network;
+        int firstLayerNeuronsCount;
+        int secondLayerNeuronsCount;
+        double learningRate;
+        int cycles;
+
         public neurosigmoid()
         {
-            
-            NeuralNetwork nn = new NeuralNetwork(21.5, new int[] { 2, 7, 6, 2 });
+            InitializeComponent();
+        }
 
-            List<double> input = new List<double>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            firstLayerNeuronsCount = int.Parse(textBox1.Text);
+            secondLayerNeuronsCount = int.Parse(textBox2.Text);
+            learningRate = double.Parse(textBox4.Text);
+            cycles = int.Parse(textBox3.Text);
+
+            
+
+            //Wejscie
+            input = new List<double>();
             input.Add(0000000000000000000);
             input.Add(0000000010100100001);
             input.Add(0010000101001000101);
@@ -48,7 +67,8 @@ namespace neurosigmoid
             input.Add(0010110111010100000);
             input.Add(0011001001001010111);
 
-            List<double> output = new List<double>();
+            //Wyjscie
+            output = new List<double>();
             output.Add(00000);
             output.Add(00001);
             output.Add(00010);
@@ -79,13 +99,153 @@ namespace neurosigmoid
             output.Add(11011);
             output.Add(11100);
 
-            nn.Train(input, output);
-            InitializeComponent();
+            network = new NeuralNetwork(learningRate, new int[] { 2, firstLayerNeuronsCount, secondLayerNeuronsCount, 2 });
+
+            //Draw structure
+            DrawNetworkStructure(networkGraph, 150, networkGraph.Height - 45, 30, 60, 60, Color.Purple, Color.Red, Color.Green);
+            updateTreeView(treeView1);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void trainButton_Click(object sender, EventArgs e)
         {
-           
+            
+            cycles = int.Parse(textBox3.Text);
+            updateTreeView(treeView1);
+
+            for (int i = 1; i <= cycles; i++)
+            {
+                network.Train(ref input, ref output);
+                Application.DoEvents();
+                textBox3.Text = i + "/" + cycles.ToString();
+                updateTreeViewValues(treeView1);
+                DrawNetworkStructure(networkGraph, 150, networkGraph.Height - 45, 30, 60, 60, Color.Purple, Color.Red, Color.Green);
+                if (i == cycles) break;
+            }
+
+        }
+
+
+        private void DrawNetworkStructure(PictureBox pb, int startX, int startY, int scale, int hspace, int vspace, Color iColor, Color hColor, Color oColor)
+        {
+            //Stworzenie bitmay i obiektu graficznego dla PictureBox
+            Bitmap b = new Bitmap(pb.Width, pb.Height);
+            Graphics g = Graphics.FromImage(b);
+
+
+            //Rysowanie
+            g.Clear(Color.White);
+
+            //Warstwy
+            for (int i = 0; i < network.Layers.Count; i++)
+            {
+                int x = startX - hspace * (network.Layers[i].Neurons.Count/2);
+                int y = startY - (vspace * i);
+
+                //Neurony
+                for (int k = 0; k < network.Layers[i].Neurons.Count ; k++)
+                {
+                    if(i == 0)
+                    {
+                        SolidBrush brush = new SolidBrush(iColor);
+                        Pen pen =  new Pen(iColor);
+                        g.DrawEllipse(pen, x, y, scale, scale);
+                        g.FillEllipse(brush, x, y, scale, scale);
+                        g.DrawString("Wy", new Font(new FontFamily("Arial"), 10f), Brushes.White, x , y);
+                    }
+                    else if (i == network.Layers.Count - 1)
+                    {
+                        SolidBrush brush = new SolidBrush(oColor);
+                        Pen pen = new Pen(oColor);
+                        g.DrawEllipse(pen, x, y, scale, scale);
+                        g.FillEllipse(brush, x, y, scale, scale);
+                        g.DrawString("We", new Font(new FontFamily("Arial"), 10f), Brushes.White, x , y);
+                    }
+                    else
+                    {
+                        SolidBrush brush = new SolidBrush(hColor);
+                        Pen pen = new Pen(hColor);
+                        g.DrawEllipse(pen, x, y, scale, scale);
+                        g.FillEllipse(brush, x, y, scale, scale);
+                        //g.DrawString(i.ToString(), new Font(new FontFamily("Arial"), 10f), Brushes.White, x, y);
+                    }
+                    
+                    if( i > 0)
+                    {
+                        int denX1 = x + (scale / 2);
+                        int denY1 = y + scale;
+                        int denX2 = (startX - hspace * (network.Layers[i - 1].Neurons.Count / 2)) + (scale / 2);
+                        int denY2 = y + vspace;
+
+                        for (int j = 0; j < network.Layers[i].Neurons[k].Dendrites.Count; j++)
+                        {
+                            Pen pen = new Pen(Color.Gray, 1.25f);
+                            g.DrawLine(pen, denX1, denY1, denX2, denY2);
+                            denX2 = denX2 + hspace;
+                            g.DrawString(network.Layers[i].Neurons[k].Dendrites[j].Weight.ToString().Substring(0,5), new Font(new FontFamily("Arial"), 8f), Brushes.Black, x+20, y+20);
+                        }
+
+
+                    }
+                    x = x + hspace;
+                }
+
+
+            }
+
+            //przypisanie grafiki do komponentu
+            pb.Image = b;
+        }
+
+        private void updateTreeView(TreeView tv)
+        {
+            tv.Nodes.Clear();
+
+            for (int i = 0; i < network.Layers.Count; i++)
+            {
+                if (i == 0) tv.Nodes.Add("Warstwa Wejsciowa ");
+                else if(i == network.Layers.Count-1) tv.Nodes.Add("Warstwa Wyjsciowa ");
+                else tv.Nodes.Add("Warstwa " + i.ToString());
+                
+                
+                for(int j = 0; j < network.Layers[i].Neurons.Count; j++)
+                {
+                    tv.Nodes[i].Nodes.Add("Neuron " + i.ToString() + "/" + j.ToString());
+                    tv.Nodes[i].Nodes[j].Nodes.Add("Delta: " + network.Layers[i].Neurons[j].Delta + " / Bias: " + network.Layers[i].Neurons[j].Bias + " / Wartość: " + network.Layers[i].Neurons[j].Value);
+                }
+
+                tv.Nodes[i].ExpandAll();
+            }
+        }
+
+        private void updateTreeViewValues(TreeView tv)
+        {
+            for (int i = 0; i < network.Layers.Count; i++)
+            {
+                for (int j = 0; j < network.Layers[i].Neurons.Count; j++)
+                {
+                    tv.Nodes[i].Nodes[j].Nodes[0].Text = "Delta: " + network.Layers[i].Neurons[j].Delta + " / Bias: " + network.Layers[i].Neurons[j].Bias + " / Wartość: " + network.Layers[i].Neurons[j].Value;
+                }
+
+                tv.Nodes[i].ExpandAll();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            firstLayerNeuronsCount = int.Parse(textBox1.Text);
+            secondLayerNeuronsCount = int.Parse(textBox2.Text);
+            learningRate = double.Parse(textBox4.Text);
+            cycles = int.Parse(textBox3.Text);
+
+            network = new NeuralNetwork(learningRate, new int[] { 2, firstLayerNeuronsCount, secondLayerNeuronsCount, 2 });
+            DrawNetworkStructure(networkGraph, 150, networkGraph.Height - 45, 20, 30, 30, Color.Purple, Color.Red, Color.Green);
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+                double [] netOutput = network.Run(input);
+                Application.DoEvents();
+                updateTreeViewValues(treeView1);
         }
     }
 }
